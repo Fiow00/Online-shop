2e285f926af98e55ebf3ab8c147d7c857c77902a
Fiow00 <ahmedyhya5325500@gmail.com>
2026-07-04 05:45:22 +0300

    Enhance admin panel to support list_display, list_filter, list_editable, prepopulated_fields for products, categories

--- shop/admin.py
+++ shop/admin.py
@@ -2,5 +2,22 @@ from django.contrib import admin
 
 from .models import Category, Product
 
-admin.site.register(Category)
-admin.site.register(Product)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name", "slug"]
+    prepopulated_fields = {"slug": ("name",)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [
+        "name",
+        "slug",
+        "price",
+        "available",
+        "created_at",
+        "updated_at",
+    ]
+    list_filter = ["available", "created_at", "updated_at"]
+    list_editable = ["price", "available"]
+    prepopulated_fields = {"slug": ("name",)}
