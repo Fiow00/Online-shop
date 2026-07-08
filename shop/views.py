796529def635191b776b6f55dfa69e069923f3e7
Fiow00 <ahmedyhya5325500@gmail.com>
2026-07-08 19:47:15 +0300

    Display Add product to cart form in product_detail

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 
+from cart.forms import CartAddProductForm
 from .models import Category, Product
 
 def product_list(request, category_slug=None):
@@ -22,7 +23,9 @@ def product_detail(request, product_id, product_slug):
     product = get_object_or_404(
         Product, id=product_id, slug=product_slug, available=True
     )
+    cart_product_form = CartAddProductForm()
 
     return render(request, "shop/product/detail.html", {
         "product": product,
+        "cart_product_form": cart_product_form,
     })
