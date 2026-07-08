cd05931b0d144f0cf6938fadbe10ced5f3d3039e
Fiow00 <ahmedyhya5325500@gmail.com>
2026-07-08 23:44:46 +0300

    Unit tests for cart_remove view
    - Test cart_remove view removes product from session cart
    - Test cart_remove view only accept post methods

--- cart/tests.py
+++ cart/tests.py
@@ -46,3 +46,45 @@ class CartAddViewTests(TestCase):
         )
 
         self.assertEqual(response.status_code, 405)
+
+
+class CartRemoveViewTests(TestCase):
+    def setUp(self):
+        self.category = Category.objects.create(
+            name="Tea",
+            slug="tea",
+        )
+
+        self.product = Product.objects.create(
+            category=self.category,
+            name="Green Tea",
+            slug="green-tea",
+            price=Decimal("10.00"),
+            available=True
+        )
+
+    def test_cart_remove_removes_product_from_cart(self):
+        self.client.post(
+            reverse("cart:cart_add", args=[self.product.id]),
+            {
+                "quantity": 2,
+                "override": False,
+            },
+        )
+
+        response = self.client.post(
+            reverse("cart:cart_remove", args=[self.product.id])
+        )
+
+        self.assertRedirects(response, reverse("cart:cart_detail"))
+
+        cart = self.client.session[settings.CART_SESSION_ID]
+
+        self.assertNotIn(str(self.product.id), cart)
+
+    def test_cart_remove_only_accepts_post(self):
+        response = self.client.get(
+            reverse("cart:cart_remove", args=[self.product.id])
+        )
+
+        self.assertEqual(response.status_code, 405)
